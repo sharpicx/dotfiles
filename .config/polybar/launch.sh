cedda297e09f9921152f867d46aabaef3c1b74f0
sharpicx <pantheisbeliefs@gmail.com>
2023-04-27 19:04:56 +0700

    Delete .config directory

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done 
-polybar -q "utama" -c /home/via/.config/polybar/config.ini &
-polybar -q "status" -c /home/via/.config/polybar/config.ini &
-polybar -q "tengah" -c /home/via/.config/polybar/config.ini &
-polybar -q "pentest" -c /home/via/.config/polybar/config.ini &
--- .config/polybar/scripts/translate.sh
+++
@@ -1,1 +0,0 @@
-python ~/random-stuff/pribadi/translatenow.py
